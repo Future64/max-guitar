75b5220e9d7b01c94d759792c6642219736d5b93
Future64 <developement.future@gmail.com>
2022-09-18 05:11:11 +0200

    dynamic build scale ok

--- src/components/strings/Strings.js
+++ src/components/strings/Strings.js
@@ -15,7 +15,50 @@ function String(props) {
   let [scaleSorted, setScaleSorted] = useState([]);
   const nbStrings = [string1, string2, string3, string4, string5, string6];
   const userInputs = props.props;
-  const majScaleKey = scales[0].majorScale.key;
+
+  /**
+   * It takes a scale and a root note, and returns a new scale with the root note at the beginning
+   * @param scale - the scale you want to use
+   * @param rootNote - the note you want to start the scale on
+   * @returns An array of notes that are in the scale.
+   */
+  const splitAndBuildScale = (scale, rootNote) => {
+    let newScale = scale.slice(scale.indexOf(rootNote));
+    let newScaleZero = scale.slice(0, scale.indexOf(rootNote));
+    let scaleFinished = [...newScale, ...newScaleZero];
+    return scaleFinished;
+  };
+
+  /**
+   * It takes a root note and an accidental, and returns a chromatic scale based on the root note and
+   * accidental
+   * @param rootNote - The root note of the scale.
+   * @param alt - "sharp" or "flat"
+   * @returns An array of notes in the chromatic scale
+   */
+  const buildNewChromaScale = (rootNote, alt) => {
+    if (alt === "#") {
+      return splitAndBuildScale(scales[0].notesSharp, rootNote);
+    } else if (alt === "b") {
+      return splitAndBuildScale(scales[0].notesFlat, rootNote);
+    } else {
+      return "Please enter a valid root note and accidentals";
+    }
+  };
+
+  /**
+   * It takes in a scaleForLive array and a chromaScale array, and returns a builtMajorScale array
+   * @param scaleForLive - an array of numbers that represent the scale you want to build.
+   * @param chromaScale - an array of all the notes in the chromatic scale
+   * @returns An array of the notes in the scale.
+   */
+  const scaleConstructor = (scaleForLive, chromaScale) => {
+    let builtMajorScale = [];
+    for (let i = 0; i < scaleForLive.length; i++) {
+      builtMajorScale.push(chromaScale[scaleForLive[i]]);
+    }
+    return builtMajorScale;
+  };
 
   function displayScale(note) {
     let classN = "hidden";
@@ -27,15 +70,19 @@ function String(props) {
     return classN;
   }
 
-  function handleScales() {
-    for (let i = 0; i < majScaleKey.length; i++) {
-      if (majScaleKey[i][0] === userInputs.note) {
-        setScaleSorted(
-          majScaleKey[i].filter(function (ele, pos) {
-            return majScaleKey[i].indexOf(ele) === pos;
-          })
-        );
-      }
+  function selectScale() {
+    if (userInputs.scale === "Pentatonique majeure") {
+      return scales[0].majorPentatonicScale;
+    } else if (userInputs.scale === "Pentatonique mineure") {
+      return scales[0].minorPentatonicScale;
+    } else if (userInputs.scale === "Gamme majeure naturel") {
+      return scales[0].naturalMajorScale;
+    } else if (userInputs.scale === "Gamme mineure naturel") {
+      return scales[0].naturalMinorScale;
+    } else if (userInputs.scale === "Gamme mineure Harmonique") {
+      return scales[0].harmonicMinorScale;
+    } else if (userInputs.scale === "Gamme mineure Mélodique") {
+      return scales[0].melodicMinorScale;
     }
   }
 
@@ -45,13 +92,19 @@ function String(props) {
     let increment1ID = 0;
     return (
       <>
-        <div className={stringClasss} key={`${stringClass}${i}${stg}`}>
-          <div className="stringReal"></div>
+        <div
+          className={stringClasss}
+          key={`${stringClass.toString()}${i.toString()}${stg.toString()}`}
+        >
+          <div
+            className="stringReal"
+            key={`${stringClass}${i}${stg}-string`}
+          ></div>
           {userInputs.alt === "#"
             ? stringClass[1][0].noteSharp.map((note, index) => {
                 return (
                   <div
-                    key={`${index}${stg}${
+                    key={`${index.toString()}${stg.toString()}${
                       stringClass[2][0].keyNumber[increment1Key++]
                     }`}
                     className="fret"
@@ -74,7 +127,7 @@ function String(props) {
             : stringClass[0].noteFlat.map((note, index) => {
                 return (
                   <div
-                    key={`${index}${stg}${
+                    key={`${index.toString()}${stg.toString()}${
                       stringClass[2][0].keyNumber[increment1Key++]
                     }`}
                     className="fret"
@@ -100,8 +153,17 @@ function String(props) {
   }
 
   useEffect(() => {
-    handleScales();
-  }, [userInputs.note]);
+    console.log(selectScale());
+
+    setScaleSorted(
+      scaleConstructor(
+        selectScale() === undefined
+          ? scales[0].majorPentatonicScale
+          : selectScale(),
+        buildNewChromaScale(userInputs.note, userInputs.alt)
+      )
+    );
+  }, [userInputs.note, userInputs.alt, userInputs.scale]);
 
   return (
     <>
@@ -118,4 +180,5 @@ function String(props) {
     </>
   );
 }
+
 export default String;
--- src/data/scalesData.js
+++ src/data/scalesData.js
@@ -1,152 +1,219 @@
 export const scales = [
   {
-    chromaticScale: {
-      build: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
-      name: "Chromatic",
-      notesSharp: [
-        "C",
-        "C#",
-        "D",
-        "D#",
-        "E",
-        "F",
-        "F#",
-        "G",
-        "G#",
-        "A",
-        "A#",
-        "B",
-      ],
-      notesFlat: [
-        "C",
-        "Db",
-        "D",
-        "Eb",
-        "E",
-        "F",
-        "Gb",
-        "G",
-        "Ab",
-        "A",
-        "Bb",
-        "B",
-      ],
-      description:
-        "The chromatic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
-    },
-    majorScale: {
-      build: [1, 1, 0.5, 1, 1, 1, 0.5],
-      name: "Major",
-      key: [
-        ["C", "D", "E", "F", "G", "A", "B"],
-        ["C#", "D#", "F", "F#", "G#", "A#", "C"],
-        ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
-        ["D", "E", "F#", "G", "A", "B", "C#"],
-        ["D#", "F", "G", "G#", "A#", "C", "D"],
-        ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
-        ["E", "F#", "G#", "A", "B", "C#", "D#"],
-        ["F", "G", "A", "Bb", "C", "D", "E"],
-        ["F#", "G#", "A#", "B", "C#", "D#", "F"],
-        ["Gb", "Ab", "Bb", "B", "Db", "Eb", "F"],
-        ["G", "A", "B", "C", "D", "E", "F#"],
-        ["G#", "A#", "C", "C#", "D#", "F", "G"],
-        ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
-        ["A", "B", "C#", "D", "E", "F#", "G#"],
-        ["A#", "C", "D", "D#", "F", "G", "A"],
-        ["Bb", "C", "D", "Eb", "F", "G", "A"],
-        ["B", "C#", "D#", "E", "F#", "G#", "A#"],
-      ],
-      notes: [
-        { C: ["C", "D", "E", "F", "G", "A", "B"] },
-        { CSharp: ["C#", "D#", "E#", "F#", "G#", "A#", "B#"] },
-        { Db: ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"] },
-        { D: ["D", "E", "F#", "G", "A", "B", "C#"] },
-        { DSharp: ["D#", "E#", "G", "G#", "A#", "B#", "D"] },
-        { Eb: ["Eb", "F", "G", "Ab", "Bb", "C", "D"] },
-        { E: ["E", "F#", "G#", "A", "B", "C#", "D#"] },
-        { F: ["F", "G", "A", "Bb", "C", "D", "E"] },
-        { FSharp: ["F#", "G#", "A#", "B", "C#", "D#", "E#"] },
-        { Gb: ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"] },
-        { G: ["G", "A", "B", "C", "D", "E", "F#"] },
-        { GSharp: ["G#", "A#", "B#", "C#", "D#", "E#", "G"] },
-        { Ab: ["Ab", "Bb", "C", "Db", "Eb", "F", "G"] },
-        { A: ["A", "B", "C#", "D", "E", "F#", "G#"] },
-        { ASharp: ["A#", "B#", "D", "D#", "E#", "G", "A"] },
-        { Bb: ["Bb", "C", "D", "Eb", "F", "G", "A"] },
-        { B: ["B", "C#", "D#", "E", "F#", "G#", "A#"] },
-      ],
-      description:
-        "The major scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
-    },
-    minorNatScale: {
-      build: [1, 0.5, 1, 1, 0.5, 1, 1],
-      name: "Minor Natural",
-      notes: ["C", "D", "D#", "F", "F#", "G", "A"],
-      description:
-        "The minor natural scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
-    },
-    minorHarmonicScale: {
-      build: [1, 0.5, 1, 1, 0.5, 1.5, 0.5],
-      name: "Minor Harmonic",
-      notes: ["C", "D", "D#", "F", "F#", "G", "A#"],
-      description:
-        "The minor harmonic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
-    },
-    minorMelodicScale: {
-      build: [1, 0.5, 1, 1, 1, 1, 0.5],
-      name: "Minor Melodic",
-      notes: ["C", "D", "D#", "F", "F#", "G", "A"],
-      description:
-        "The minor melodic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
-    },
-  },
-];
-// export default scales;
+    // gamme pentatonique et hexatonique
+    minorPentatonicScale: [0, 3, 5, 7, 10],
+    majorPentatonicScale: [0, 2, 4, 7, 9],
+    majorPentatonicBluesScale: [0, 2, 3, 7, 9],
+    minorPentatonicBluesScale: [0, 3, 5, 7, 10],
 
-export const modes = [
-  {
-    ionian: {
-      build: [1, 1, 0.5, 1, 1, 1, 0.5],
-      name: "Ionian",
-      description:
-        "The ionian scale is the default scale for the ionian mode. It is a major scale with the first note being a major third and the last note being a major seventh.",
-    },
-    dorian: {
-      buitld: [1, 0.5, 1, 1, 1, 0.5, 1],
-      name: "Dorian",
-      description:
-        "Dorian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the dominant.",
-    },
-    phrygian: {
-      build: [0.5, 1, 1, 1, 0.5, 1, 1],
-      name: "Phrygian",
-      description:
-        "Phrygian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
-    },
-    lydian: {
-      build: [1, 1, 1, 0.5, 1, 1, 0.5],
-      name: "Lydian",
-      description:
-        "Lydian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the superdominant.",
-    },
-    mixolydian: {
-      build: [1, 1, 0.5, 1, 1, 0.5, 1],
-      name: "Mixolydian",
-      description:
-        "Mixolydian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
-    },
-    aeolian: {
-      build: [1, 0.5, 1, 1, 0.5, 1, 1],
-      name: "Aeolian",
-      description:
-        "Aeolian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
-    },
-    locrian: {
-      build: [0.5, 1, 1, 0.5, 1, 1, 1],
-      name: "Locrian",
-      description:
-        "Locrian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
-    },
+    // gamme hexatonique
+    majorBluesScale: [0, 2, 3, 4, 7, 9],
+    minorBluesScale: [0, 3, 5, 6, 7, 10],
+
+    // Gamme naturelle
+    chromaticScale: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    naturalMajorScale: [0, 2, 4, 5, 7, 9, 11],
+    naturalMinorScale: [0, 2, 3, 5, 7, 8, 10],
+    harmonicMinorScale: [0, 2, 3, 5, 7, 8, 11],
+    melodicMinorScale: [0, 2, 3, 5, 7, 9, 11],
+
+    // Mode grec
+    ionianScale: [0, 2, 3, 5, 7, 8, 10],
+    dorianScale: [0, 2, 3, 5, 7, 9, 10],
+    phrygianScale: [0, 1, 3, 5, 7, 8, 10],
+    lydianScale: [0, 2, 4, 6, 7, 9, 11],
+    mixolydianScale: [0, 2, 4, 5, 7, 9, 10],
+    aeolianScale: [0, 2, 3, 5, 7, 8, 10],
+    minorLocrianScale: [0, 1, 3, 5, 6, 8, 10],
+
+    // Gamme symétrique
+    wholeToneScale: [0, 2, 4, 6, 8, 10],
+    diminishedScale: [0, 2, 3, 5, 6, 8, 9, 11],
+    augmentedScale: [0, 3, 4, 7, 8, 11],
+
+    // Gamme éxotique
+    majorHarmonicMinorScale: [0, 2, 3, 5, 7, 8, 11],
+
+    // Chromatic brut
+    notesSharp: [
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#",
+      "A",
+      "A#",
+      "B",
+    ],
+    notesFlat: [
+      "C",
+      "Db",
+      "D",
+      "Eb",
+      "E",
+      "F",
+      "Gb",
+      "G",
+      "Ab",
+      "A",
+      "Bb",
+      "B",
+    ],
   },
 ];
-// export default [scales, modes];
+
+// export const scales = [
+//   {
+//     chromaticScale: {
+//       build: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+//       name: "Chromatic",
+//       notesSharp: [
+//         "C",
+//         "C#",
+//         "D",
+//         "D#",
+//         "E",
+//         "F",
+//         "F#",
+//         "G",
+//         "G#",
+//         "A",
+//         "A#",
+//         "B",
+//       ],
+//       notesFlat: [
+//         "C",
+//         "Db",
+//         "D",
+//         "Eb",
+//         "E",
+//         "F",
+//         "Gb",
+//         "G",
+//         "Ab",
+//         "A",
+//         "Bb",
+//         "B",
+//       ],
+//       description:
+//         "The chromatic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
+//     },
+//     majorScale: {
+//       build: [1, 1, 0.5, 1, 1, 1, 0.5],
+//       name: "Major",
+//       key: [
+//         ["C", "D", "E", "F", "G", "A", "B"],
+//         ["C#", "D#", "F", "F#", "G#", "A#", "C"],
+//         ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
+//         ["D", "E", "F#", "G", "A", "B", "C#"],
+//         ["D#", "F", "G", "G#", "A#", "C", "D"],
+//         ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+//         ["E", "F#", "G#", "A", "B", "C#", "D#"],
+//         ["F", "G", "A", "Bb", "C", "D", "E"],
+//         ["F#", "G#", "A#", "B", "C#", "D#", "F"],
+//         ["Gb", "Ab", "Bb", "B", "Db", "Eb", "F"],
+//         ["G", "A", "B", "C", "D", "E", "F#"],
+//         ["G#", "A#", "C", "C#", "D#", "F", "G"],
+//         ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+//         ["A", "B", "C#", "D", "E", "F#", "G#"],
+//         ["A#", "C", "D", "D#", "F", "G", "A"],
+//         ["Bb", "C", "D", "Eb", "F", "G", "A"],
+//         ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+//       ],
+//       notes: [
+//         { C: ["C", "D", "E", "F", "G", "A", "B"] },
+//         { CSharp: ["C#", "D#", "E#", "F#", "G#", "A#", "B#"] },
+//         { Db: ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"] },
+//         { D: ["D", "E", "F#", "G", "A", "B", "C#"] },
+//         { DSharp: ["D#", "E#", "G", "G#", "A#", "B#", "D"] },
+//         { Eb: ["Eb", "F", "G", "Ab", "Bb", "C", "D"] },
+//         { E: ["E", "F#", "G#", "A", "B", "C#", "D#"] },
+//         { F: ["F", "G", "A", "Bb", "C", "D", "E"] },
+//         { FSharp: ["F#", "G#", "A#", "B", "C#", "D#", "E#"] },
+//         { Gb: ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"] },
+//         { G: ["G", "A", "B", "C", "D", "E", "F#"] },
+//         { GSharp: ["G#", "A#", "B#", "C#", "D#", "E#", "G"] },
+//         { Ab: ["Ab", "Bb", "C", "Db", "Eb", "F", "G"] },
+//         { A: ["A", "B", "C#", "D", "E", "F#", "G#"] },
+//         { ASharp: ["A#", "B#", "D", "D#", "E#", "G", "A"] },
+//         { Bb: ["Bb", "C", "D", "Eb", "F", "G", "A"] },
+//         { B: ["B", "C#", "D#", "E", "F#", "G#", "A#"] },
+//       ],
+//       description:
+//         "The major scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
+//     },
+//     minorNatScale: {
+//       build: [1, 0.5, 1, 1, 0.5, 1, 1],
+//       name: "Minor Natural",
+//       notes: ["C", "D", "D#", "F", "F#", "G", "A"],
+//       description:
+//         "The minor natural scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
+//     },
+//     minorHarmonicScale: {
+//       build: [1, 0.5, 1, 1, 0.5, 1.5, 0.5],
+//       name: "Minor Harmonic",
+//       notes: ["C", "D", "D#", "F", "F#", "G", "A#"],
+//       description:
+//         "The minor harmonic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
+//     },
+//     minorMelodicScale: {
+//       build: [1, 0.5, 1, 1, 1, 1, 0.5],
+//       name: "Minor Melodic",
+//       notes: ["C", "D", "D#", "F", "F#", "G", "A"],
+//       description:
+//         "The minor melodic scale is the most common scale in music. It is a scale with all the notes of the chromatic scale.",
+//     },
+//   },
+// ];
+// // export default scales;
+
+// export const modes = [
+//   {
+//     ionian: {
+//       build: [1, 1, 0.5, 1, 1, 1, 0.5],
+//       name: "Ionian",
+//       description:
+//         "The ionian scale is the default scale for the ionian mode. It is a major scale with the first note being a major third and the last note being a major seventh.",
+//     },
+//     dorian: {
+//       buitld: [1, 0.5, 1, 1, 1, 0.5, 1],
+//       name: "Dorian",
+//       description:
+//         "Dorian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the dominant.",
+//     },
+//     phrygian: {
+//       build: [0.5, 1, 1, 1, 0.5, 1, 1],
+//       name: "Phrygian",
+//       description:
+//         "Phrygian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
+//     },
+//     lydian: {
+//       build: [1, 1, 1, 0.5, 1, 1, 0.5],
+//       name: "Lydian",
+//       description:
+//         "Lydian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the superdominant.",
+//     },
+//     mixolydian: {
+//       build: [1, 1, 0.5, 1, 1, 0.5, 1],
+//       name: "Mixolydian",
+//       description:
+//         "Mixolydian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
+//     },
+//     aeolian: {
+//       build: [1, 0.5, 1, 1, 0.5, 1, 1],
+//       name: "Aeolian",
+//       description:
+//         "Aeolian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
+//     },
+//     locrian: {
+//       build: [0.5, 1, 1, 0.5, 1, 1, 1],
+//       name: "Locrian",
+//       description:
+//         "Locrian mode is a scale that starts with a 2nd and 3rd degree of the tonic, and then follows the same pattern of intervals. It is a major scale with the added note of the subdominant.",
+//     },
+//   },
+// ];
